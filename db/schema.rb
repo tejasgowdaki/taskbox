12e009a788d9921db86e7e926081ea8a2d55eeaf
tejasgowdaki <tejasgowda80@gmail.com>
2016-12-08 16:09:42 +0530

    7-12-16 front end

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114133447) do
+ActiveRecord::Schema.define(version: 20161129052332) do
 
   create_table "announcements", force: :cascade do |t|
     t.string   "title"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20161114133447) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
